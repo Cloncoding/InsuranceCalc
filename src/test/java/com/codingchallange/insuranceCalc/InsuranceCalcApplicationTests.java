9ea329d9496a0952218bae8d8a486076a1645902
Cloncoding <crystel2@gmx.de>
2024-12-05 22:33:55 +0100

    Initial commit

---
+++ src/test/java/com/codingchallange/insuranceCalc/InsuranceCalcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codingchallange.insuranceCalc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InsuranceCalcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
