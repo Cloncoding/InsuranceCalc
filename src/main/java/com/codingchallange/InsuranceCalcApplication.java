b8d1ad0b4ebca1aa454e889250a7cdc61ecce3fd
Cloncoding <crystel2@gmx.de>
2024-12-08 23:29:58 +0100

    Added new Spring startup

---
+++ src/main/java/com/codingchallange/InsuranceCalcApplication.java
@@ -0,0 +1,15 @@
+package com.codingchallange;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InsuranceCalcApplication
+{
+	public static void main(String[] args)
+	{
+		SpringApplication.run(InsuranceCalcApplication.class, args);
+		
+		System.out.println("Running");
+	}
+}
